0db28e05134435cf3b5487fbe63372a8f452c668
Maziar <maziar.gomrokchi@system1.com>
2023-06-15 18:53:05 +0000

    adding RL Agent and  Experiment Config

--- adcraft/experiment_utils/experiment_configs.py
+++ adcraft/experiment_utils/experiment_configs.py
@@ -16,86 +16,86 @@ MAX_DAYS = 60
 
 
 dense_env_config = dict(
-        keyword_config={
-            "outer_directory": Path.cwd().as_posix(),
-            "mean_volume": 128,
-            "conversion_rate": 0.8,
-            "make_quant_func": make_experiment_quantiles,
-            "load_quant_func": load_experiment_quantiles
-            },
-            num_keywords = NUM_KEYWORDS,
-            max_days = MAX_DAYS,
-            updater_params = [["vol", 0.03], ["ctr", 0.03], ["cvr", 0.03]],
-            updater_mask = None
-            )
+    keyword_config={
+        "outer_directory": Path.cwd().as_posix(),
+        "mean_volume": 128,
+        "conversion_rate": 0.8,
+        "make_quant_func": make_experiment_quantiles,
+        "load_quant_func": load_experiment_quantiles
+        },
+        num_keywords = NUM_KEYWORDS,
+        max_days = MAX_DAYS,
+        updater_params = [["vol", 0.03], ["ctr", 0.03], ["cvr", 0.03]],
+        updater_mask = None
+        )
 
 semi_dense_env_config = dict(
-        keyword_config={
-            "outer_directory": Path.cwd().as_posix(),
-            "mean_volume": 64,
-            "conversion_rate": 0.8,
-            "make_quant_func": make_experiment_quantiles,
-            "load_quant_func": load_experiment_quantiles
-            },
-            num_keywords = NUM_KEYWORDS,
-            max_days = MAX_DAYS,
-            updater_params = [["vol", 0.03], ["ctr", 0.03], ["cvr", 0.03]],
-            updater_mask = None #[True]*NUM_KEYWORDS
-            )
+    keyword_config={
+        "outer_directory": Path.cwd().as_posix(),
+        "mean_volume": 64,
+        "conversion_rate": 0.8,
+        "make_quant_func": make_experiment_quantiles,
+        "load_quant_func": load_experiment_quantiles
+        },
+        num_keywords = NUM_KEYWORDS,
+        max_days = MAX_DAYS,
+        updater_params = [["vol", 0.03], ["ctr", 0.03], ["cvr", 0.03]],
+        updater_mask = None #[True]*NUM_KEYWORDS
+        )
 
 sparse_env_config = dict(
-        keyword_config={
-            "outer_directory": Path.cwd().as_posix(),
-            "mean_volume": 64,
-            "conversion_rate": 0.1,
-            "make_quant_func": make_experiment_quantiles,
-            "load_quant_func": load_experiment_quantiles
-            },
-            num_keywords = NUM_KEYWORDS,
-            max_days = MAX_DAYS,
-            updater_params = [["vol", 0.03], ["ctr", 0.03], ["cvr", 0.03]],
-            updater_mask = None #[True]*NUM_KEYWORDS
-            )
+    keyword_config={
+        "outer_directory": Path.cwd().as_posix(),
+        "mean_volume": 64,
+        "conversion_rate": 0.1,
+        "make_quant_func": make_experiment_quantiles,
+        "load_quant_func": load_experiment_quantiles
+        },
+        num_keywords = NUM_KEYWORDS,
+        max_days = MAX_DAYS,
+        updater_params = [["vol", 0.03], ["ctr", 0.03], ["cvr", 0.03]],
+        updater_mask = None #[True]*NUM_KEYWORDS
+        )
 
 very_sparse_env_config = dict(
-        keyword_config={
-            "outer_directory": Path.cwd().as_posix(),
-            "mean_volume": 16,
-            "conversion_rate": 0.1,
-            "make_quant_func": make_experiment_quantiles,
-            "load_quant_func": load_experiment_quantiles
-            },
-            num_keywords = NUM_KEYWORDS,
-            max_days = MAX_DAYS,
-            updater_params = [["vol", 0.03], ["ctr", 0.03], ["cvr", 0.03]],
-            updater_mask = None #[True]*NUM_KEYWORDS
-            )
+    keyword_config={
+        "outer_directory": Path.cwd().as_posix(),
+        "mean_volume": 16,
+        "conversion_rate": 0.1,
+        "make_quant_func": make_experiment_quantiles,
+        "load_quant_func": load_experiment_quantiles
+        },
+        num_keywords = NUM_KEYWORDS,
+        max_days = MAX_DAYS,
+        updater_params = [["vol", 0.03], ["ctr", 0.03], ["cvr", 0.03]],
+        updater_mask = None #[True]*NUM_KEYWORDS
+        )
 
 
 non_stationary_dense_env_config = dict(
-        keyword_config={
-            "outer_directory": Path.cwd().as_posix(),
-            "mean_volume": 128,
-            "conversion_rate": 0.8,
-            "make_quant_func": make_experiment_quantiles,
-            "load_quant_func": load_experiment_quantiles
-            },
-            num_keywords = NUM_KEYWORDS,
-            max_days = MAX_DAYS,
-            updater_params = [["vol", 0.03], ["ctr", 0.03], ["cvr", 0.03]],
-            updater_mask = [True]*NUM_KEYWORDS
-            )
+    keyword_config={
+        "outer_directory": Path.cwd().as_posix(),
+        "mean_volume": 128,
+        "conversion_rate": 0.8,
+        "make_quant_func": make_experiment_quantiles,
+        "load_quant_func": load_experiment_quantiles
+        },
+        num_keywords = NUM_KEYWORDS,
+        max_days = MAX_DAYS,
+        updater_params = [["vol", 0.03], ["ctr", 0.03], ["cvr", 0.03]],
+        updater_mask = [True]*NUM_KEYWORDS
+        )
 
 non_stationary_sparse_env_config = dict(
-        keyword_config={
-            "outer_directory": Path.cwd().as_posix(),
-            "mean_volume": 64,
-            "conversion_rate": 0.1,
-            "make_quant_func": make_experiment_quantiles,
-            "load_quant_func": load_experiment_quantiles
-            },
-            num_keywords = NUM_KEYWORDS,
-            max_days = MAX_DAYS,
-            updater_params = [["vol", 0.03], ["ctr", 0.03], ["cvr", 0.03]],
-            updater_mask = [True]*NUM_KEYWORDS
-            )
+    keyword_config={
+        "outer_directory": Path.cwd().as_posix(),
+        "mean_volume": 64,
+        "conversion_rate": 0.1,
+        "make_quant_func": make_experiment_quantiles,
+        "load_quant_func": load_experiment_quantiles
+        },
+        num_keywords = NUM_KEYWORDS,
+        max_days = MAX_DAYS,
+        updater_params = [["vol", 0.03], ["ctr", 0.03], ["cvr", 0.03]],
+        updater_mask = [True]*NUM_KEYWORDS
+        )
